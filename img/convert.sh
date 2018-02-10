5f020753a4ca41bda6ca322bbb3415cc371dac61
balaji.sj <sjbalajimdu@gmail.com>
2018-02-10 18:13:19 +0530

    basic added

---
+++ img/convert.sh
@@ -0,0 +1,4 @@
+convert classroom1.jpg -resize 450x300\! classroom1.1.jpg
+convert classroomTeaching1.jpg -resize 450x300\! classroomTeaching1.1.jpg
+convert personalAttention1.jpg -resize 450x300\! personalAttention1.1.jpg
+convert personalAttention2.jpg -resize 1280x813\! personalAttention2.1.jpg
