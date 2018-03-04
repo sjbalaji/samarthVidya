bb61ae00e90ec82656f1da8bec1a1ac703ffee95
balaji.sj <sjbalajimdu@gmail.com>
2018-03-04 11:51:00 +0530

    basic things done

--- img/convert.sh
+++ img/convert.sh
@@ -2,3 +2,16 @@ convert classroom1.jpg -resize 450x300\! classroom1.1.jpg
 convert classroomTeaching1.jpg -resize 450x300\! classroomTeaching1.1.jpg
 convert personalAttention1.jpg -resize 450x300\! personalAttention1.1.jpg
 convert personalAttention2.jpg -resize 585x400\! personalAttention2.1.jpg
+convert classroomTeaching2.jpg -resize 585x400\! classroomTeaching2.1.jpg
+
+convert classroomTeaching1.jpg -resize 557x418\! classroomTeaching1.2.jpg
+convert classroomTeaching2.jpg -resize 557x418\! classroomTeaching2.2.jpg
+convert classroomTeaching3.jpg -resize 557x418\! classroomTeaching3.2.jpg
+
+convert personalAttention1.jpg -resize 557x418\! personalAttention1.2.jpg
+convert personalAttention2.jpg -resize 557x418\! personalAttention2.2.jpg
+convert studyEnvironment.jpg -resize 557x418\! personalAttention3.2.jpg
+
+convert classroom1.jpg -resize 557x418\! classroom1.2.jpg
+convert classroom2.jpg -resize 557x418\! classroom2.2.jpg
+convert classroom3.jpg -resize 557x418\! classroom3.2.jpg
